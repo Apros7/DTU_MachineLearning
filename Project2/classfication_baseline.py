255b4a36cb3efe731bc83ae665ced721f1b5b129
apros7 <lucas.vilsen@gmail.com>
2023-11-07 17:29:45 +0100

    classification baseline works

--- Project2/classfication_baseline.py
+++ Project2/classfication_baseline.py
@@ -2,24 +2,18 @@
 
 from automation import Tester
 import numpy as np
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
 from collections import Counter
 import torch
 
 def baseline(x_train, x_test, y_train, y_test, func_var):
-
-    y_test = np.argmax(y_test, axis=1).tolist()
-    y_train = np.argmax(y_train, axis=1).tolist()
-
-    most_frequent_class = Counter(y_train).most_common(1)[0][0]
-    y_pred = [most_frequent_class] * len(y_test)
-
-    f1score = f1_score(np.array(y_test), np.array(y_pred))
-    print(f1score)
-
-    return f1score
+    most_common_class = max(set(tuple(row) for row in y_train), key=y_train.tolist().count)
+    y_pred = [most_common_class] * len(y_test)
+    f1 = f1_score(y_test, y_pred, average=func_var)
+    return f1
   
 
 if __name__ == "__main__":
     path_to_data = "/Users/lucasvilsen/Desktop/DTU/MachineLearning&DataMining/Project2/StandardizedDataFrameWithNansFilled.csv"
-    tester = Tester("StatusClassification", path_to_data, function_to_test = baseline, final_test = False, k = 10)
+    averages_to_test = ['micro', 'macro', 'weighted', 'samples']
+    tester = Tester("StatusClassification", path_to_data, function_to_test = baseline, final_test = False, k = 10, vars_to_test=averages_to_test)
